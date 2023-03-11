17c54b7617bda50c73695ca68213ab60ad1b55e4
Horacio Musser <horacio.musser@urbetrack.com>
2023-03-11 15:04:38 -0300

    Project 02

---
+++ React/02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,19 @@
+console.log('Hola Mundo');
+
+//Variables y constantes
+const nombre = 'Horacio';
+const apellido = 'Musser';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if(true){
+    let valorDado = 8; //---> let y const permiten crear variables de scope o scopeadas.
+    console.log(valorDado);
+    const nombre = 'Felipe'; //---> let y const permiten crear variables de scope o scopeadas.
+    console.log(nombre);
+}
+
+console.log(valorDado);

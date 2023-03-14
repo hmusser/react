c1ad88831195d6eea1693a19a5e6445cbd68a30d
Horacio <musserhoracio@gmail.com>
2023-03-13 23:25:48 -0300

    05-Funciones

---
+++ React/02-intro-javascript/src/bases/05-funciones.js
@@ -0,0 +1,58 @@
+//Funciones en JavaScript
+//Asignar las funciones a constantes para que no admitan reasignaciones de otros valores como por ejemplo:
+//saludar = 30;
+const saludar = function( nombre ){
+    return `Hola ${nombre}`;
+}
+
+//Funciones de flecha ó anónimas:
+const saludar2 = ( nombre ) => {
+    return `Hola ${nombre}`;
+}
+
+//Si devuelvo un solo valor puedo:
+const saludar3 = ( nombre ) => `Hola ${nombre}`;
+
+const saludar4 = ( ) => `Hola Mundo!`;
+
+const getUser = () => {
+    return {
+        uid: '213421asd',
+        userName: 'El_Papi_Chulo',
+    }
+}
+
+//Puedo reemplazar el return por:
+const getUser2 = () => (
+        {
+        uid: '213421asd',
+        userName: 'El_Papi_Chulo2',
+    }
+    );
+
+console.log(saludar('Amelia'));
+console.log(saludar2('Musser'));
+console.log(saludar3('Blasco'));
+console.log(saludar4());
+
+console.log(getUser());
+console.log(getUser2());
+
+
+//TAREA
+//1) Transformar a una función de flecha.
+//2) Tiene que retornar...
+/*function getUsuarioActivo( nombre ){
+    return {
+        uid: 'ABC876',
+        username: nombre,
+    }
+};
+*/
+const usuarioActivo = ( nombre ) => ({
+        uid: 'ABC876',
+        username: nombre,
+    });
+
+
+console.log(usuarioActivo('The Hulk'));

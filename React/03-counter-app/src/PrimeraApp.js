4037c8da0a4f389199c4e015b8ed8814c2050510
Horacio Musser <horacio.musser@urbetrack.com>
2023-03-22 18:12:46 -0300

    04-Primeros Pasos en React

---
+++ React/03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+//Los componontes pueden estar basados en clases y en funciones.
+//Aqui usaremos las funciones.
+/** FUNCTIONAL COMPONENTS */
+const PrimeraApp = () =>
+{
+return <h1>Hola Mundo.</h1>; //jsx
+}
+
+export default PrimeraApp;
